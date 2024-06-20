b7d747c66993acc3e62d56921c2545fd857c1810
YusraEjaz123 <163528893+YusraEjaz123@users.noreply.github.com>
2024-06-21 02:26:46 +0500

    Add files via upload

---
+++ basicfundamentals.php
@@ -0,0 +1,151 @@
+<?php
+
+//comments
+
+// echo "Yusra Ejaz";
+// datatypes in php
+// $string = "hellow to my world";
+// $int = 18;
+// $float = 52.2;
+// $bool = true; //false
+// $arr =array("apple", "mango","cherry");
+
+//string functions
+// echo "hellow world";
+// echo strlen("hello world") . "<br>";
+// echo str-word-count("hello to my world");
+// echo strrev("hello girl");
+// echo strpos("hello girl", "traveling the world");
+// echo str-replace("women","lady","living the life fully and happily")
+
+//php constant
+//define("name","YUsra");
+// echo name;
+
+//if else
+// $num = 18;
+// if($num >0){
+//      echo "positive number";
+// }else{
+//      echo "nagative number";  
+// }
+
+// $islogin = true;
+// if($islogin){
+//      echo "welcome back";
+// }else{
+//      echo "please log in";  
+// }
+
+// $score =55;
+// if($score >= 80){
+//      echo "Grade A";  
+// }else if($score >= 70){
+// echo "Grade B";
+// }else if($score >= 60){
+//      echo "Grade C";
+// }else{
+//      echo "FAIL";
+// }
+
+// CHECKING STRING LENGTH
+// $msg = "";
+// if(strlen($msg)>0){
+//  echo "message is empty";
+// }
+
+//CHECKING ARRAY ELEMENTS
+// $fruits = array("apple", "mango","cherry");
+
+// if(in-array("mango",$fruits)){
+//      echo "fruit is available";
+// }else{
+//  echo "fruit is not available";
+// }
+
+//using logical operator
+// $age = 15;
+// $hasId = true;
+// if($age >= 18 && $hasId){
+//      echo "you are allowed to enter";
+// }else{
+//      echo "you are not allowed to enter";
+// }
+
+//switch statement
+// $favColor = "pink";
+// switch($favColor){
+//     case "red":
+//         echo "your fav color is " . $favColor;
+//         break;
+//     case "yellow":
+//         echo "your fav color is " . $favColor;
+//         break;
+
+//     default:
+//         echo "your fav color is not available but it is " .$favColor;
+//     }
+
+// for loop
+// for ($i=0; $i <= 10; $i=$i+2) {
+//     // echo "$i".") Esha Ejaz"."<br>";
+//     echo ($i+1);
+// }
+
+//while loop
+
+// $i = 1;
+// while($i <= 5){
+//     echo "$i".") Esha Ejaz"."<br>";
+//     $i++;
+//     }
+
+// for each loop
+//    $fruits = array("apple", "mango","cherry");
+//    foreach ($fruits as $fruits){
+//     echo "$fruit <br>";
+//    }
+
+//indexed arrays
+// $fruits = array("apple", "mango","cherry");
+// echo "I like $fruits[0],$fruits[1],$fruits[2];
+
+// Associative Array
+// $age = array("yusra" => 23,
+//             "eiman" => 22,
+//             "Waleed" => 26
+// );
+// echo "yusra is " . $age["waleed"]. "years old";
+
+// 2d Array
+// $cars = array (
+//     array("toyota", 22, 10),
+//     array("Honda", 3, 20),
+//     array("Suzuki", 44, 30)
+// );
+// echo $cars[0][0]. ": in stock: ". $cars[0][1]. ", sold: ". $cars[0][2]. "<br>";
+// echo $cars[1][0]. ": in stock: ". $cars[1][1]. ", sold: ". $cars[1][2]. "<br>";
+// echo $cars[2][0]. ": in stock: ". $cars[2][1]. ", sold: ". $cars[2][2]. "<br>";
+
+// basic function
+// function say(){
+//     echo "hi";
+// }
+// say();
+// say();
+
+//paramererized function
+// function add($num1, $num2){
+//     return $num1 + $num2;
+// }
+// //echo add(10,4)
+// $a = add(14,8);
+// echo "the sum is $a";
+
+// dafault function
+
+// function weight($w=40){
+//     echo "the weight is $w";
+// }
+// weight(90);
+?>
---
+++ form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form</title>
+</head>
+<body>
+    <form action="welcome.php" method="get ">
+        Name: <input type="text" name="name" id="">
+        Email: <input type="email" name="email" id="">
+        Age: <input type="number" name="age" id="">
+        <input type="submit" value="save">
+    </form>
+</body>
+</html>
